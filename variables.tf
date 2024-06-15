ee92edb199213576908193959e85d8a52a7d605b
huyftb <manhhuyftb@gmail.com>
2024-06-15 10:41:31 +0700

    update docker

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "asia-east1"
+}
+
+variable "github_repository" {
+    default = "Finly"
+}
+
+variable "github_owner" {
+    default = "huyftb"
+}
+
+variable "github_branch" {
+    default = "^back-end-dash$"
+}
